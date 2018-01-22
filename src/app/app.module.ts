06e1810c153a4f45659772b0995f332c948914dc
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-22 21:37:03 +0100

    root component update w/ includes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { ListComponent } from './components/board/list/list.component';
+import { CardComponent } from './components/board/list/card/card.component';
+import { DataService } from './common/services/data.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    ListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
