30357c1429478c7842ccfd70176944e11830c257
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-04-22 13:21:11 +0200

    basic routing w/ imports in main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardRootComponent } from './feature/board/components/board-root/board-root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: BoardRootComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: BoardRootComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { BoardModule } from './feature/board/board.module';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     CoreModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BoardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
