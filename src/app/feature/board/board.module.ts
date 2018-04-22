59c48c657012117343b17d504d3e61821c660d2e
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-04-22 23:10:18 +0200

    shared module utilised in board module

--- src/app/feature/board/board.module.ts
+++ src/app/feature/board/board.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../../shared/shared.module';
 import { BoardRootComponent } from './components/board-root/board-root.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule
   ],
   declarations: [BoardRootComponent]
 })
