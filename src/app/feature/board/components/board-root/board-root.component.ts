25ee24f3072d3d37950e5afb6ae717f4e3feaae9
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-04-23 21:42:50 +0200

    board crud w/ shell

--- src/app/feature/board/components/board-root/board-root.component.ts
+++ src/app/feature/board/components/board-root/board-root.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../../../core/services/http.service';
 
 @Component({
   selector: 't-board-root',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-root.component.scss']
 })
 export class BoardRootComponent implements OnInit {
+  boards: any[] = [];
+  board: Object = {};
+  boardName: string = 'xem';
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
 
   ngOnInit() {
+    this.getBoards();
+  }
+
+  getBoards(): void {
+    this.httpService.getBoards()
+      .subscribe(data => {
+        this.boards = data.data;
+      },
+        error => console.log(error)
+      );
+  }
+
+  onBoardClick(id: number): void {
+    this.httpService
+      .getBoard(id)
+      .subscribe(
+        res => this.board = res.data,
+        error => this.errorMessage = <any>error
+      );
+  }
+
+  createBoard() {
+    this.httpService.createBoard('vuk')
+      .subscribe(res => {
+        debugger;
+      });
+  }
+
+  editBoard(id, name): void {
+    this.httpService.editBoard(id, name)
+      .subscribe(
+        res => {
+          this.board = res;
+          this.getBoards();
+        },
+        error => this.errorMessage = <any>error
+      );
   }
 
 }
