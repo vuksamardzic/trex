fafb704122c33247b6c96ba7aa5d1525d36df12c
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-04-25 12:47:37 +0200

    board-content init

---
+++ src/app/feature/board/components/board-content/board-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 't-board-content',
+  templateUrl: './board-content.component.html',
+  styleUrls: ['./board-content.component.scss']
+})
+export class BoardContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
