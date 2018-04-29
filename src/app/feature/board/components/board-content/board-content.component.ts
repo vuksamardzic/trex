4e33faf3fbcaf5e3b0bb8aff8a6e4498bf1a1b20
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-04-29 19:56:51 +0200

    Board content shell, style w/ functionality

--- src/app/feature/board/components/board-content/board-content.component.ts
+++ src/app/feature/board/components/board-content/board-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BoardCrudService } from '../../../../core/services/board-crud.service';
 
 @Component({
   selector: 't-board-content',
@@ -6,10 +7,33 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./board-content.component.scss']
 })
 export class BoardContentComponent implements OnInit {
+  @Input() board;
+  editing = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private boardCrudService: BoardCrudService) {
   }
 
+  ngOnInit(): void {
+  }
+
+  editBoard(): void {
+    if (this.editing) {
+      this.editing = false;
+      this.boardCrudService.loader = true;
+      this.boardCrudService.execEditBoard(this.board.id, this.board.name);
+    }
+  }
+
+  checkEnterKeypress(ev: KeyboardEvent): void {
+    if (ev.charCode === 13) {
+      this.editing = false;
+      this.boardCrudService.loader = true;
+      this.boardCrudService.execEditBoard(this.board.id, this.board.name);
+    }
+  }
+
+  deleteBoard(): void {
+    this.boardCrudService.loader = true;
+    this.boardCrudService.execDeleteBoard(this.board.id);
+  }
 }
