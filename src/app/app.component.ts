fa750dd59f8b4441349ce4f24761b27eda71c6dd
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-05-08 23:32:31 +0200

    loader on routing set.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 't-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent { }
+export class AppComponent {
+  loading = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    } else if (
+      routerEvent instanceof NavigationError ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationEnd
+    ) {
+      this.loading = false;
+    }
+  }
+}
