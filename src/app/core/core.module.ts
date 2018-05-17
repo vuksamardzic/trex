d6a250dcb4b23d7c29c49dde24d3f962df9673ce
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-05-17 23:12:06 +0200

    Header routing w/ unit test.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { ConfigService } from './services/config.service';
 import { BoardCrudService } from './services/board-crud.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   providers: [
     ConfigService,
